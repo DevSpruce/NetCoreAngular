9cafd0ac88e2aef25880f06593fda9c50de97117
Mateusz Świerczyński <swierk94@wp.pl>
2021-02-07 18:22:53 +0100

    Utworzenie projektu Portal Randkowy web API

--- PortalRandkowy.API/Controllers/ValuesController.cs
+++ PortalRandkowy.API/Controllers/ValuesController.cs
@@ -15,7 +15,7 @@ namespace PortalRandkowy.API.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {                        
-            return new string[] { "value3", "value2" };
+            return new string[] { "value3", "value2","value3" };
         }
 
         // GET api/values/5
