22b8fcddbea55f2c88f20b92354c6b2d3f3afe22
Mateusz Świerczyński <swierk94@wp.pl>
2021-02-11 21:44:14 +0100

    Add value.component

--- PortalRandkowy.SPA/src/app/app.module.ts
+++ PortalRandkowy.SPA/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { ValueComponent } from './value/value.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+      ValueComponent,
+      ValueComponent
+   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
