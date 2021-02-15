330d84431cce20c8a2c643292705aa4fb4ffee04
Mateusz Świerczyński <swierk94@wp.pl>
2021-02-15 21:37:14 +0100

    Logging to console informations about user is logged in or not

--- PortalRandkowy.SPA/src/app/nav/nav.component.ts
+++ PortalRandkowy.SPA/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
+import { AuthService } from './../_services/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { error } from '@angular/compiler/src/util';
 
 @Component({
   selector: 'app-nav',
@@ -9,13 +11,17 @@ export class NavComponent implements OnInit {
 
   model: any = {};
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
   login(){
-    console.log(this.model);
+    this.authService.login(this.model).subscribe(next => {
+      console.log('Zalogowałeś się do aplikacji');
+    }, error => {
+      console.log('Wystąpił błąd logowania');
+    });
   }
 
 }
